abac92dcffb45030fbf7e13edbc9a1c2b18b53fc
PhyuCin <ayemyaphyucin@my.jcu.edu.au>
2020-01-26 15:19:41 +0800

    finish future plans

--- themes/vital/home.php
+++ themes/vital/home.php
@@ -14,29 +14,29 @@
 			
 				<div class="width25">
 					<div class="tickerInnerContent">
-						<h3 class="tickerUp">5</h3>
-						<p>Regular Villages Adopted</p> 
+						<h3 class="tickerUp">0</h3>
+						<p>Ticker 1</p> 
 					</div><!-- .tickerInnerContent -->
 				</div><!-- .width25 -->
 				
 				<div class="width25">
 					<div class="tickerInnerContent">
-						<h3 class="tickerUp">497</h3>
-						<p>Patients seen till date with regular follow up camps</p> 
+						<h3 class="tickerUp">0</h3>
+						<p>Ticker 2</p> 
 					</div><!-- .tickerInnerContent -->
 				</div><!-- .width25 -->
 				
 				<div class="width25">
 					<div class="tickerInnerContent">
-						<h3 class="tickerUp">110</h3>
-						<p>Average patients screened per medical camp</p> 
+						<h3 class="tickerUp">0</h3>
+						<p>Ticker 3</p> 
 					</div><!-- .tickerInnerContent -->
 				</div><!-- .width25 -->
 				
 				<div class="width25">
 					<div class="tickerInnerContent">
-						<h3 class="tickerUp">6</h3>
-						<p>Specialities offered</p> 
+						<h3 class="tickerUp">0</h3>
+						<p>Ticker 4</p> 
 					</div><!-- .tickerInnerContent -->
 				</div><!-- .width25 -->
 				
@@ -46,22 +46,73 @@
 		</div><!-- .container -->
 	</div><!-- .sectionWhite -->
 	
+	<div class="clear"></div><!-- .clear -->
 	
 	<div class="section sectionThemeColor">
 		<div class="container">
+			<div class="services">
 			<div class="centerText">
-				<h2>Future Plans</h2>
-			</div><!-- .centerText -->
-		
+					<h2>Services Provided</h2>
+				</div><!-- .centerText -->
+				
+				<div class= "servicesbox">
+					<div class="servicesBox1">
+						<h3>Services 1</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+					</div><!-- .servicesBox1 -->
+					
+					<div class="servicesBox2">
+						<h3>Services 2</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+					</div><!-- .servicesBox2 -->
+					
+					<div class="servicesBox3">
+						<h3>Services 3</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+					</div><!-- .servicesBox3 -->	
+				</div><!-- .servicesBox -->	
+				
+				<div class="servicesImage">
+				<p>qwerfghjk</p>
+				</div><!-- .servicesImage -->	
 
+			</div><!-- .services -->
 		</div><!-- .container -->
 	</div><!-- .sectionThemeColor -->
+	
+	<div class="clear"></div><!-- .clear -->
 	
 	<div class="section sectionWhite">
 		<div class="container">
 		
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+	<div class="plans">
+		
+				<div class="centerText">
+					<h2>Future Plans</h2>
+				</div><!-- .centerText -->
+				
+				<div class="plansBox plansBox1">
+					<h3>Plan 1</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+				</div><!-- .plansBox1 -->
+				
+				<div class="plansBox plansBox2">
+					<h3>Plan 2</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+				</div><!-- .plansBox2 -->
+
+				<div class="plansBox plansBox3">
+					<h3>Plan 3</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+				</div><!-- .plansBox3 -->
+
+				<div class="plansBox plansBox4">
+					<h3>Plan 4</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+				</div><!-- .plansBox4 -->
+	
+			
+		</div><!-- .plans -->
 
 		
 
@@ -73,13 +124,8 @@
 	<div class="section sectionThemeColor">
 		<div class="container">
 		
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-
 		
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		
 	</div><!-- .container -->
 	</div><!-- .sectionThemeColor -->
 	
